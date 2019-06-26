29b7df493aa8968d388cbb5b56995f53ff343564
carlos-andre-kindle <carlos.andre@agenciakindle.com.br>
2019-06-26 14:36:47 -0300

    criando as rotas e os componentes de navegação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,42 @@
 import { Component } from '@angular/core';
+import { fadeAnimation } from './animations';
+import {
+  trigger,
+  query,
+  style,
+  animate,
+  group,
+  animateChild,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  //animations: [fadeAnimation]
+  animations: [
+    trigger('myAnimation', [
+      transition('* => *', [
+        query(
+          ':enter',
+          [style({ opacity: 0 })],
+          { optional: true }
+        ),
+        query(
+          ':leave',
+           [style({ opacity: 1 }), animate('0.3s', style({ opacity: 0 }))],
+          { optional: true }
+        ),
+        query(
+          ':enter',
+          [style({ opacity: 0 }), animate('0.3s', style({ opacity: 1 }))],
+          { optional: true }
+        )
+      ])
+    ]),
+
+    ]
 })
 export class AppComponent {
   title = 'site-portfolio';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContatoComponent } from './contato/contato.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'competencias', component: SkillsComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'contato', component: ContatoComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    HomeComponent,
+    SkillsComponent,
+    PortfolioComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
