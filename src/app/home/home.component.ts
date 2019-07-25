9934067a27bef123d57b5ccabb0594d2b7248d9e
Carlos André <carlosdev@users.noreply.github.com>
2019-07-25 01:24:07 -0300

    importando e habilitando o animejs na home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import anime from '../../../node_modules/animejs/lib/anime.es.js';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  textWrapper = document.querySelector('.letters');
+  @ViewChild('title') textWrapper: ElementRef;
 
-  ngOnInit() {
+  constructor() {
   }
 
+  teste() {
+    console.log(this.textWrapper);
+    this.textWrapper.nativeElement.innerHTML = this.textWrapper.nativeElement.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+    anime.timeline({loop: true})
+  .add({
+    targets: '.ml10 .letter',
+    rotateY: [-90, 0],
+    duration: 1300,
+    delay: function(el, i) {
+      return 45 * i;
+    }
+  }).add({
+    targets: '.ml10',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+  }
+  ngOnInit() {
+  }
 }
