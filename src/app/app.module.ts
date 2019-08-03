9c6f3b4d6f0eef69fefe61b7f3220541675d67a6
Carlos André <carlosdev@users.noreply.github.com>
2019-08-03 01:45:37 -0300

    upando arquivos de configuração do contact form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { Routes, RouterModule } from '@angular/router';
+import { HttpService } from './Shared/http.service';
+import { Material } from './app-material';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { HomeComponent } from './home/home.component';
@@ -31,6 +35,10 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    Material,
     AppRoutingModule,
     RouterModule.forRoot(appRoutes)
   ],
--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from "../Shared/http.service";
+import { FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-contato',
@@ -6,10 +8,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contato.component.css']
 })
 export class ContatoComponent implements OnInit {
+  loading = false;
+  buttionText = 'Enviar';
 
-  constructor() { }
+  constructor(public http: HttpService) { }
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email
+  ]);
+
+  nameFormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(2)
+  ]);
+
+  commentFormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(10)
+  ]);
 
   ngOnInit() {
+    console.log(this.http.test);
   }
 
+  register() {
+    this.loading = true;
+    this.buttionText = 'Enviando...';
+    let user = {
+      name: this.nameFormControl.value,
+      email: this.emailFormControl.value,
+      comment: this.commentFormControl.value
+    };
+    this.http.sendEmail("http://localhost:3000/sendmail", user).subscribe(
+      data => {
+        let res: any = data; 
+        console.log(
+          `👏 > 👏 > 👏 > 👏 ${user.name} is successfully register and mail has been sent and the message id is ${res.messageId}`
+        );
+      },
+      err => {
+        console.log(err);
+        this.loading = false;
+        this.buttionText = 'Enviar';
+      }, () => {
+        this.loading = false;
+        this.buttionText = 'Enviar';
+      }
+    );
+  }
 }
